53558246ce02bc49586fd2368c4209d9ca6d215b
Sebastian Fidura <sebastian@fidura.pl>
2018-09-28 11:48:49 +0200

    gotowe zadanie

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//var number = document.getElementById("input").value;
+
+
+function myFunction(n) {
+    
+    var result;
+    
+    
+    if (n >= 100 && n <= 200) {
+//        result = console.log(n);
+        result = console.log("Liczba znajduje się w przedziale");
+    } else {
+//        result = console.log(n);
+        result = console.log("Liczba nie znajduje się w przedziale");
+    }
+    return result;
+}
